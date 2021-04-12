d9ae56b4fbc4834a1b8b6a3c80edacfc5b38d3bd
meitalpa <74905597+meitalpa@users.noreply.github.com>
2021-04-12 17:51:29 +0300

    Update loop-test.py

--- loop-test.py
+++ loop-test.py
@@ -2,7 +2,7 @@ import time
 import os
 print("round 1")
 for i in range (10):
-    f= open("output/file-%d.txt" % (i+1),"w+") 
+    f= open("output/bla-file-%d.txt" % (i+1),"w+") 
     print("file-%d.txt Created!" % (i+1))
     for i in range(5):
      f.write("This is line %d\r\n" % (i+1))
@@ -12,7 +12,7 @@ for i in range (10):
 time.sleep(300)
 print("round 2")
 for i in range (10):
-    f= open("output/file-%d.txt" % (i+1),"w+") 
+    f= open("output/bla-fileeee-%d.txt" % (i+1),"w+") 
     print("file-%d.txt Created!" % (i+1))
     for i in range(5):
      f.write("This is line %d\r\n" % (i+1))
