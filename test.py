bb7a37a2ed2d73c55df314950377e6eaabf3ef74
meitalpa <74905597+meitalpa@users.noreply.github.com>
2021-05-27 17:59:42 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 #model_path = '/cnvrg/output/frt_model_random_forest.sav'
 #model_path = '/output/frt_model_random_forest.sav'
-model_path = 'output/frt_model_random_forest.sav'
+model_path = '/cnvrg/output/frt_model_random_forest.sav'
 model = pickle.load(open(model_path, 'rb'))
 
 def predict(created_hr,day_of_week,epoch,category,other = None):
