04f42ecc739cf30fac4ed534dc215c1ab80b8546
meitalpa <74905597+meitalpa@users.noreply.github.com>
2021-05-30 17:50:41 +0300

    Update test.py

--- test.py
+++ test.py
@@ -8,10 +8,9 @@ import time
 #model_path = '/cnvrg/output/frt_model_random_forest.sav'
 #model_path = '/output/frt_model_random_forest.sav'
 model_path = '/cnvrg/output/frt_model_random_forest.sav'
-model = pickle.load(open(model_path, 'rb'))
 
 def predict(created_hr,day_of_week,epoch,category,other = None):
-    
+    model = pickle.load(open(model_path, 'rb'))
     data = [[created_hr,day_of_week,epoch,category]]
     df = pd.DataFrame(data, columns = ['created_hr','day_of_week','epoch','category'])
     rfc_predict = model.predict(df)
