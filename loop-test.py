de936662c7c12fefb3382a8b41408e3d3a03b8b3
meitalpa <74905597+meitalpa@users.noreply.github.com>
2021-08-19 17:41:08 +0300

    Update loop-test.py

--- loop-test.py
+++ loop-test.py
@@ -9,12 +9,12 @@ for i in range (10):
     f.close()
     time.sleep(10)
     
-time.sleep(300)
-print("round 2")
-for i in range (10):
-    f= open("output/bla-fileeee-%d.txt" % (i+1),"w+") 
-    print("file-%d.txt Created!" % (i+1))
-    for i in range(5):
-     f.write("This is line %d\r\n" % (i+1))
-    f.close()
-    time.sleep(10)
+# time.sleep(300)
+# print("round 2")
+# for i in range (10):
+#     f= open("output/bla-fileeee-%d.txt" % (i+1),"w+") 
+#     print("file-%d.txt Created!" % (i+1))
+#     for i in range(5):
+#      f.write("This is line %d\r\n" % (i+1))
+#     f.close()
+#     time.sleep(10)
