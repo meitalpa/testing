e5682761f1e032d242093dc862906bafaf8d8298
meitalpa <74905597+meitalpa@users.noreply.github.com>
2022-08-14 14:45:43 +0300

    Update create-files.py

--- create-files.py
+++ create-files.py
@@ -1,6 +1,6 @@
 for i in range (10):
-    f = open("output/file-%d.txt" % (i+1),"w+") 
-    print("file-%d.txt Created!" % (i+1))
+    f = open("output/fileeeee-%d.txt" % (i+1),"w+") 
+    print("fileeeee-%d.txt Created!" % (i+1))
     for i in range(5):
      f.write("This is line %d\r\n" % (i+1))
     f.close()
