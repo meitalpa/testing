a3e3f7cebe94daabee0cd0492a19decea3c366bc
meitalpa <74905597+meitalpa@users.noreply.github.com>
2022-08-17 13:47:14 +0300

    Update create-files.py

--- create-files.py
+++ create-files.py
@@ -1,11 +1,13 @@
 import time
-for i in range (10):
+for i in range (3):
     ts = time.time()
     f = open("output/file-{}-{}.txt".format(ts,i+1),"w+") 
     print("file-{}-{}.txt Created!".format(ts,i+1))
     for i in range(5):
      f.write("This is line %d\r\n" % (i+1))
     f.close()
+    time.sleep(500)
+
     
     
    
